58780a75b273f6bac0914bec8f73167b66708664
llanes37 <78422866+llanes37@users.noreply.github.com>
2025-10-25 17:59:45 +0200

    subida

--- 01_cadenas.py
+++
@@ -1,5 +0,0 @@
-cadena = 'Esto es una cadena de texto'
-lista_palabras = cadena.split(" ")
-print(lista_palabras)
-# La funcion len(lista) devuelve el nmero de elementos de una lista.
-print("La cadena de texto tiene:", len(lista_palabras), "palabras.")
